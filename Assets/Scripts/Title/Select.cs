1c4be4fa9a49b030170233a901690dd05891dcd0
houtaro <kdyt0128@gmail.com>
2021-01-20 02:34:07 +0900

    SE and BGM
    Enabled to set the SE and BGM

--- Assets/Scripts/Title/Select.cs
+++ Assets/Scripts/Title/Select.cs
@@ -3,10 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 public class Select : MonoBehaviour
 {
-    public Image image1; 
+    public Image image1;
     public Image image2;
+    public AudioClip start;
+    public AudioClip exit;
+    AudioSource audioSource;
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
     public void SelectStart()
     {
         image1.rectTransform.localPosition = new Vector3(80, -140, 0);
@@ -19,14 +27,41 @@ public class Select : MonoBehaviour
     }
     public void GameStart()
     {
-        SceneManager.LoadScene("Main");
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(start);
+            StartCoroutine(Checking(audioSource, () =>
+            {
+                SceneManager.LoadScene("Main");
+            }));
+        }
     }
     public void GameExit()
     {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(exit);
+            StartCoroutine(Checking(audioSource, () =>
+            {
+
 #if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
+                UnityEditor.EditorApplication.isPlaying = false;
 #else
-    Application.Quit();
+                Application.Quit();
 #endif
+            }));
+        }
+    }
+    private IEnumerator Checking(AudioSource audio, UnityAction callback)
+    {
+        while (true)
+        {
+            yield return new WaitForFixedUpdate();
+            if (!audio.isPlaying)
+            {
+                callback();
+                break;
+            }
+        }
     }
 }
