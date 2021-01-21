acd6f087546ed9aaaee20b5f41b90cc620883360
asihdt <asihdt@outlook.jp>
2021-01-22 05:51:07 +0900

    Stage1作成

---
+++ Assets/Scripts/Editor/CreateAutoPieceData.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreateAutoPieceData : MonoBehaviour
+{
+    private const int CreateMaxGexSize = 7;
+    
+    /*
+    [MenuItem("Tools/CreateAllPieceData")]
+    public static void CreateAllPieceData()
+    {
+        
+    }
+    */
+    
+}
